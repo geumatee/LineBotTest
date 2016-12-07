04a4ef07c976f1f62f8dc7e6d482e45d2aef48e3
GUIDE-PC\geuma <matee@hlpth.com>
2016-12-07 14:08:22 +0700

    implement Validate Token

--- routes/verify.js
+++ routes/verify.js
@@ -1,15 +1,34 @@
 /*eslint no-undef: "error"*/
 /*eslint-env node*/
 
-var app = require('express');
-var router = app.Router();
+var express = require('express');
+var router = express.Router();
+
+var request = require('request');
+
+// var app = express();
 // var bodyParser = require('body-parser');
 // var multer = require('multer'); // v1.0.5
 // var upload = multer(); // for parsing multipart/form-data
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    var headers = {
+        'Authorization': 'Bearer r66gB/QeQB9YKgvhT9QZoXmRuf0VIIIfsKiszE6+Qa0P2goun2p1hqBhuJwlTZNA5VOeojEffX95TJB162tqBXNWLxfuQzVlfuThpbzhtPhs9HddCEtj0+GxlJXufpEMAdHAhuu0INpJZxZudiYbYAdB04t89/1O/w1cDnyilFU='
+    };
+
+    var options = {
+        url: 'https://api.line.me/v1/oauth/verify',
+        headers: headers
+    };
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body);
+        }
+    }
+
+    request(options, callback);
 });
 
 module.exports = router;
