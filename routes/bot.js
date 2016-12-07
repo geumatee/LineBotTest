0acdb70c914d063b86b36940f68bb0c7cc7a6338
GUIDE-PC\geuma <matee@hlpth.com>
2016-12-07 16:03:50 +0700

    add res.send in bot

--- routes/bot.js
+++ routes/bot.js
@@ -48,6 +48,7 @@ router.post('/',function(req, res){
 
                 request(options, function (error, response, body) {
                     console.log("respond " + error + " " + JSON.stringify(response) + " " + JSON.stringify(body));
+                    res.send(JSON.stringify(response));
                     if (!error && response.statusCode == 200) {
                         console.log(body);
                     }
